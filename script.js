e8f0c6d9a1ff340874043edf8788ac337eada3a0
lasbella09123-pixel <lasbella09123@gmail.com>
2026-07-02 12:30:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
 
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
